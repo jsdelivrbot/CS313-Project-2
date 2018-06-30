5d12992af692dfc01f0a95b99683922bc8f1f44a
Jason Pyle <pyl13002@byui.edu>
2018-06-30 15:46:45 -0600

    Added db support

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function getPrice(req, res) {
 
 function storeQuery(query) {
 	var sql = 'INSERT INTO item (searchItem, ts) VALUES ("' + query + '", CURRENT_TIMESTAMP';
-
+	console.log("DB WORK");
 	pool.query(sql, function(err, result) {
 		if (err) {
 			console.log(err);
